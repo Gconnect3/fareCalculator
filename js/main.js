5fb19a3af0156404d44f311635d790a457104b49
GeorgeT <90588054+Gconnect3@users.noreply.github.com>
2022-08-22 15:53:21 -0400

    Add extra money, updated calculations

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 document.querySelector(".money");
+document.querySelector(".extra");
 document.querySelector(".fare");
 let calculate = document.querySelector(".btn");
 let answer = document.querySelector(".answer");
 
 class Calculator{
-    constructor(moneyOnCard, fareRate, displayAnswer){
+    constructor(moneyOnCard, fareRate, extra, displayAnswer){
         this._money = +(moneyOnCard.toFixed(2)) || 0;
         this._fare = +(fareRate.toFixed(2)) || 2.75;
+        this._extra = +(extra.toFixed(2) || 0)
         this._ans = displayAnswer;
     }
 
@@ -16,13 +18,16 @@ class Calculator{
     get fareRate(){
         return this._fare
     }
+    get extra(){
+        return this._extra
+    }
     get displayAnswer(){
         return this._ans
     }
 
     compute(){
         if (isNaN(this.moneyOnCard) || isNaN(this.fareRate) || this.moneyOnCard < 0 || this.fareRate < 0) return;
-        let money = this.moneyOnCard * 100;
+        let money = (this.extra + this.moneyOnCard) * 100;
         let fare  = this.fareRate * 100;
         let diff = 0
         
@@ -30,7 +35,7 @@ class Calculator{
             if(indx % fare === 0){diff = indx - money; break;}
         };
 
-        let addMoney = diff / 100;
+        let addMoney = (diff / 100) + this.extra;
         let totalRides = (this.moneyOnCard + addMoney) / this.fareRate;
         let addMplusRide = addMoney + this.fareRate;
 
@@ -51,13 +56,14 @@ calculate.addEventListener("click", button =>{
     // get the values from the input
     let money = Number(document.querySelector(".money").value);
     let fare = Number(document.querySelector(".fare").value);
+    let extra = Number(document.querySelector(".extra").value)
 
     //create an instance of Calculator class with our input values
-    let calculator = new Calculator(money, fare, answer);
+    let calculator = new Calculator(money, fare, extra, answer);
 
     //use the method to compute the data
     calculator.compute();
 
     //clear money input value
-    document.querySelector(".money").value = ""
+    document.querySelector(".money").value = "";
 })
